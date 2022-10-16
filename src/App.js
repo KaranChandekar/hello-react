116edd7ef82b06d28469ac5057e93a816ca02b5c
Karan <karanchandekar431@gmail.com>
2022-10-16 23:10:21 +0530

    intro to useState Hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import "./App.css";
 import Employee from "./Components/Employee";
+import { useState } from "react";
 
 function App() {
   const showEmployees = true;
+  const [role, setRole] = useState("Dev");
 
   return (
     <div className="App">
+      <input
+        type="text"
+        onChange={(e) => {
+          console.log(e.target.value);
+          setRole(e.target.value);
+        }}
+      />
       {showEmployees ? (
         <div>
           <Employee name="Karan" role="Intern" />
-          <Employee name="Abby" />
+          <Employee name="Abby" role={role} />
           <Employee name="John" />
         </div>
       ) : (
