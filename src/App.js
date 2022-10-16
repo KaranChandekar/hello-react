5d98326da708938e543a070fc16b5355b64833ca
Karan <karanchandekar431@gmail.com>
2022-10-16 20:50:06 +0530

    creating first component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import './App.css';
+import "./App.css";
+import Employee from "./Components/Employee";
 
 function App() {
+  console.log("Here we loging an Employee!");
+  const showEmployees = true;
+
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      {console.log(showEmployees)};
+      {showEmployees ? (
+        <div>
+          <Employee />
+          <Employee />
+          <Employee />
+          <Employee />
+          <Employee />
+        </div>
+      ) : (
+        <p>You cannot see the employees!</p>
+      )}
     </div>
   );
 }
---
+++ src/Components/Employee.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Employee = () => {
+  return <div>Here is an Employee!</div>;
+};
+
+export default Employee;
