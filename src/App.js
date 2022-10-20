e34f4c17b0cd2419ad1c5ccc583985bc155b2750
Karan <karanchandekar431@gmail.com>
2022-10-20 22:11:59 +0530

    styling react with tailwind css classes

--- src/App.js
+++ src/App.js
@@ -7,20 +7,61 @@ function App() {
   const [role, setRole] = useState("Dev");
 
   return (
-    <div className="App bg-red-100">
-      <input
-        type="text"
-        onChange={(e) => {
-          console.log(e.target.value);
-          setRole(e.target.value);
-        }}
-      />
+    <div className="App">
       {showEmployees ? (
-        <div>
-          <Employee name="Karan" role="Intern" />
-          <Employee name="Abby" role={role} />
-          <Employee name="John" />
-        </div>
+        <>
+          <input
+            type="text"
+            onChange={(e) => {
+              console.log(e.target.value);
+              setRole(e.target.value);
+            }}
+          />
+          <div className="flex flex-wrap justify-center">
+            <Employee
+              name="Karan"
+              role="Intern"
+              img="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?cs=srgb&dl=pexels-italo-melo-2379005.jpg&fm=jpg&_gl=1*fbpl6t*_ga*NTY1NTI3NzIuMTY2NjE5MTEwNQ..*_ga_8JE65Q40S6*MTY2NjI4MzY3Ni4yLjEuMTY2NjI4Mzg4MC4wLjAuMA.."
+            />
+            <Employee
+              name="Abby"
+              role={role}
+              img="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg&_gl=1*mhmkkg*_ga*NTY1NTI3NzIuMTY2NjE5MTEwNQ..*_ga_8JE65Q40S6*MTY2NjE5MTExMi4xLjEuMTY2NjE5MTIyOC4wLjAuMA.."
+            />
+            <Employee
+              name="John"
+              img="https://images.pexels.com/photos/1844547/pexels-photo-1844547.jpeg?cs=srgb&dl=pexels-dalila-dalprat-1844547.jpg&fm=jpg&_gl=1*clpdqa*_ga*NTY1NTI3NzIuMTY2NjE5MTEwNQ..*_ga_8JE65Q40S6*MTY2NjI4MzY3Ni4yLjEuMTY2NjI4NDAyNS4wLjAuMA.."
+            />
+            <Employee
+              name="Karan"
+              role="Intern"
+              img="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?cs=srgb&dl=pexels-italo-melo-2379005.jpg&fm=jpg&_gl=1*fbpl6t*_ga*NTY1NTI3NzIuMTY2NjE5MTEwNQ..*_ga_8JE65Q40S6*MTY2NjI4MzY3Ni4yLjEuMTY2NjI4Mzg4MC4wLjAuMA.."
+            />
+            <Employee
+              name="Abby"
+              role={role}
+              img="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg&_gl=1*mhmkkg*_ga*NTY1NTI3NzIuMTY2NjE5MTEwNQ..*_ga_8JE65Q40S6*MTY2NjE5MTExMi4xLjEuMTY2NjE5MTIyOC4wLjAuMA.."
+            />
+            <Employee
+              name="John"
+              img="https://images.pexels.com/photos/1844547/pexels-photo-1844547.jpeg?cs=srgb&dl=pexels-dalila-dalprat-1844547.jpg&fm=jpg&_gl=1*clpdqa*_ga*NTY1NTI3NzIuMTY2NjE5MTEwNQ..*_ga_8JE65Q40S6*MTY2NjI4MzY3Ni4yLjEuMTY2NjI4NDAyNS4wLjAuMA.."
+            />
+            <Employee
+              name="Karan"
+              role="Intern"
+              img="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?cs=srgb&dl=pexels-italo-melo-2379005.jpg&fm=jpg&_gl=1*fbpl6t*_ga*NTY1NTI3NzIuMTY2NjE5MTEwNQ..*_ga_8JE65Q40S6*MTY2NjI4MzY3Ni4yLjEuMTY2NjI4Mzg4MC4wLjAuMA.."
+            />
+            <Employee
+              name="Abby"
+              role={role}
+              img="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg&_gl=1*mhmkkg*_ga*NTY1NTI3NzIuMTY2NjE5MTEwNQ..*_ga_8JE65Q40S6*MTY2NjE5MTExMi4xLjEuMTY2NjE5MTIyOC4wLjAuMA.."
+            />
+            <Employee
+              name="John"
+              img="https://images.pexels.com/photos/1844547/pexels-photo-1844547.jpeg?cs=srgb&dl=pexels-dalila-dalprat-1844547.jpg&fm=jpg&_gl=1*clpdqa*_ga*NTY1NTI3NzIuMTY2NjE5MTEwNQ..*_ga_8JE65Q40S6*MTY2NjI4MzY3Ni4yLjEuMTY2NjI4NDAyNS4wLjAuMA.."
+            />
+          </div>
+        </>
       ) : (
         <p>You cannot see the employees!</p>
       )}
--- src/Components/Employee.jsx
+++ src/Components/Employee.jsx
@@ -2,10 +2,21 @@ import React from "react";
 
 const Employee = (props) => {
   return (
-    <div>
-      <h1>Here is an Employee {props.name}!</h1>
-      {/* <p>{props.role ? props.role : "No role"}</p> */}
-      {props.role ? <p className="role">{props.role}</p> : <p className="no-role">No Role</p>}
+    <div class="py-8 px-8 max-w-sm m-4 bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+      <img
+        class="object-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
+        src={props.img}
+        alt="Man's Face"
+      />
+      <div class="text-center space-y-2 sm:text-left">
+        <div class="space-y-0.5">
+          <p class="text-lg text-black font-semibold">{props.name}</p>
+          <p class="text-slate-500 font-medium">{props.role}</p>
+        </div>
+        <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+          Message
+        </button>
+      </div>
     </div>
   );
 };
