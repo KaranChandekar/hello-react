63ac1471dde682f7979299ecea5de800970874f2
Karan <karanchandekar431@gmail.com>
2022-11-13 18:39:22 +0530

    map through state array

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
 import "./index.css";
 import Employee from "./Components/Employee";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const showEmployees = true;
   const [role, setRole] = useState("Dev");
+
+  const [employees, setEmployees] = useState([
+    {
+      name: "Karan",
+      role: "Intern",
+      img: "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg",
+    },
+    {
+      name: "Abby",
+      role: "Dev",
+      img: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg",
+    },
+    {
+      name: "John",
+      role: "Director of Eng.",
+      img: "https://images.pexels.com/photos/1844547/pexels-photo-1844547.jpeg",
+    },
+    {
+      name: "Sal",
+      role: "Manager",
+      img: "https://images.pexels.com/photos/3586798/pexels-photo-3586798.jpeg",
+    },
+    {
+      name: "Melanie",
+      role: "Software Engineer",
+      img: "https://images.pexels.com/photos/1840608/pexels-photo-1840608.jpeg",
+    },
+    {
+      name: "Murat",
+      role: "Web Designer",
+      img: "https://images.pexels.com/photos/3283568/pexels-photo-3283568.jpeg",
+    },
+    {
+      name: "Brandon",
+      role: "The Devops Guy",
+      img: "https://images.pexels.com/photos/2741701/pexels-photo-2741701.jpeg",
+    },
+    {
+      name: "Corey",
+      role: "Senior",
+      img: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg",
+    },
+    {
+      name: "Jean",
+      role: "Software Developer",
+      img: "https://images.pexels.com/photos/1239288/pexels-photo-1239288.jpeg",
+    },
+  ]);
 
   return (
     <div className="App">
@@ -18,48 +67,16 @@ function App() {
             }}
           />
           <div className="flex flex-wrap justify-center">
-            <Employee
-              name="Karan"
-              role="Intern"
-              img="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?cs=srgb&dl=pexels-italo-melo-2379005.jpg&fm=jpg&_gl=1*fbpl6t*_ga*NTY1NTI3NzIuMTY2NjE5MTEwNQ..*_ga_8JE65Q40S6*MTY2NjI4MzY3Ni4yLjEuMTY2NjI4Mzg4MC4wLjAuMA.."
-            />
-            <Employee
-              name="Abby"
-              role={role}
-              img="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg&_gl=1*mhmkkg*_ga*NTY1NTI3NzIuMTY2NjE5MTEwNQ..*_ga_8JE65Q40S6*MTY2NjE5MTExMi4xLjEuMTY2NjE5MTIyOC4wLjAuMA.."
-            />
-            <Employee
-              name="John"
-              img="https://images.pexels.com/photos/1844547/pexels-photo-1844547.jpeg?cs=srgb&dl=pexels-dalila-dalprat-1844547.jpg&fm=jpg&_gl=1*clpdqa*_ga*NTY1NTI3NzIuMTY2NjE5MTEwNQ..*_ga_8JE65Q40S6*MTY2NjI4MzY3Ni4yLjEuMTY2NjI4NDAyNS4wLjAuMA.."
-            />
-            <Employee
-              name="Karan"
-              role="Intern"
-              img="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?cs=srgb&dl=pexels-italo-melo-2379005.jpg&fm=jpg&_gl=1*fbpl6t*_ga*NTY1NTI3NzIuMTY2NjE5MTEwNQ..*_ga_8JE65Q40S6*MTY2NjI4MzY3Ni4yLjEuMTY2NjI4Mzg4MC4wLjAuMA.."
-            />
-            <Employee
-              name="Abby"
-              role={role}
-              img="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg&_gl=1*mhmkkg*_ga*NTY1NTI3NzIuMTY2NjE5MTEwNQ..*_ga_8JE65Q40S6*MTY2NjE5MTExMi4xLjEuMTY2NjE5MTIyOC4wLjAuMA.."
-            />
-            <Employee
-              name="John"
-              img="https://images.pexels.com/photos/1844547/pexels-photo-1844547.jpeg?cs=srgb&dl=pexels-dalila-dalprat-1844547.jpg&fm=jpg&_gl=1*clpdqa*_ga*NTY1NTI3NzIuMTY2NjE5MTEwNQ..*_ga_8JE65Q40S6*MTY2NjI4MzY3Ni4yLjEuMTY2NjI4NDAyNS4wLjAuMA.."
-            />
-            <Employee
-              name="Karan"
-              role="Intern"
-              img="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?cs=srgb&dl=pexels-italo-melo-2379005.jpg&fm=jpg&_gl=1*fbpl6t*_ga*NTY1NTI3NzIuMTY2NjE5MTEwNQ..*_ga_8JE65Q40S6*MTY2NjI4MzY3Ni4yLjEuMTY2NjI4Mzg4MC4wLjAuMA.."
-            />
-            <Employee
-              name="Abby"
-              role={role}
-              img="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg&_gl=1*mhmkkg*_ga*NTY1NTI3NzIuMTY2NjE5MTEwNQ..*_ga_8JE65Q40S6*MTY2NjE5MTExMi4xLjEuMTY2NjE5MTIyOC4wLjAuMA.."
-            />
-            <Employee
-              name="John"
-              img="https://images.pexels.com/photos/1844547/pexels-photo-1844547.jpeg?cs=srgb&dl=pexels-dalila-dalprat-1844547.jpg&fm=jpg&_gl=1*clpdqa*_ga*NTY1NTI3NzIuMTY2NjE5MTEwNQ..*_ga_8JE65Q40S6*MTY2NjI4MzY3Ni4yLjEuMTY2NjI4NDAyNS4wLjAuMA.."
-            />
+            {employees.map((employee) => {
+              return (
+                <Employee
+                  key={uuidv4()}
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img}
+                />
+              );
+            })}
           </div>
         </>
       ) : (
