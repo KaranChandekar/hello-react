c4f2832b193ca002d21190feb9d089fd20cd89e4
Karan <karanchandekar431@gmail.com>
2022-11-13 18:56:07 +0530

    created a popup modal with react bootstrap

--- src/Components/Employee.jsx
+++ src/Components/Employee.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import EditEmployee from "./EditEmployee";
 
 const Employee = (props) => {
   return (
@@ -13,9 +14,7 @@ const Employee = (props) => {
           <p className="text-lg text-black font-semibold">{props.name}</p>
           <p className="text-slate-500 font-medium">{props.role}</p>
         </div>
-        <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
-          Update
-        </button>
+        <EditEmployee />
       </div>
     </div>
   );
