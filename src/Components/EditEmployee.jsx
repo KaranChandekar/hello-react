b8cd3fbb99aad09c4f32ae536a400e8cd001fe69
Karan <karanchandekar431@gmail.com>
2022-11-14 19:35:57 +0530

    prefill form data in modal

--- src/Components/EditEmployee.jsx
+++ src/Components/EditEmployee.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
-import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
-function EditEmployee() {
+function EditEmployee(props) {
+  const [name, setName] = useState(props.name);
+  const [role, setRole] = useState(props.role);
+
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
@@ -42,7 +44,10 @@ function EditEmployee() {
                   className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
                   id="name"
                   type="text"
-                  value="Jane Doe"
+                  value={name}
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
                 />
               </div>
             </div>
@@ -60,7 +65,10 @@ function EditEmployee() {
                   className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
                   id="role"
                   type="text"
-                  value="Jane Doe"
+                  value={role}
+                  onChange={(e) => {
+                    setRole(e.target.value);
+                  }}
                 />
               </div>
             </div>
@@ -69,13 +77,13 @@ function EditEmployee() {
         <Modal.Footer>
           <button
             onClick={handleClose}
-            class="bg-slate-400 hover:bg-slate-500 text-white py-2 px-4 rounded"
+            className="bg-slate-400 hover:bg-slate-500 text-white py-2 px-4 rounded"
           >
             Close
           </button>
           <button
             form="editmodal"
-            class="bg-purple-600 hover:bg-purple-700 text-white py-2 px-4 rounded"
+            className="bg-purple-600 hover:bg-purple-700 text-white py-2 px-4 rounded"
           >
             Update
           </button>
