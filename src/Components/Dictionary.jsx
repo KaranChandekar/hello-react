0a6c6b984a386d079d852be76945373a018a4419
Karan <karanchandekar431@gmail.com>
2022-12-27 11:43:08 +0530

    useEffect dependency array explained

--- src/Components/Dictionary.jsx
+++ src/Components/Dictionary.jsx
@@ -3,10 +3,19 @@ import { useState, useEffect } from "react";
 
 const Dictionary = () => {
   const [word, setWord] = useState("");
+  const [word2, setWord2] = useState("");
 
   useEffect(() => {
     console.log("State updated ", word);
-  }); // limit what state useEffect cares about --> dependency array
+  }, [word]);
+
+  useEffect(() => {
+    console.log("State updated ", word2);
+  }, [word2]);
+
+  // no dependency array --> update for any state change
+  // empty dependency array --> execute once
+  // passing in data --> only execute when those state variables are changed
 
   return (
     <>
@@ -16,7 +25,14 @@ const Dictionary = () => {
         onChange={(e) => {
           setWord(e.target.value);
         }}
-      ></input>
+      />
+      <h1>Let's find the definition for {word2}</h1>
+      <input
+        type="text"
+        onChange={(e) => {
+          setWord2(e.target.value);
+        }}
+      />
     </>
   );
 };
