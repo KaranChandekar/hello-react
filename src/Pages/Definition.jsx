c39e2904dbb92bf121b5286a5572428a47ffbe60
Karan <karanchandekar431@gmail.com>
2022-12-31 20:08:40 +0530

    fetch response status codes and errors

--- src/Pages/Definition.jsx
+++ src/Pages/Definition.jsx
@@ -9,32 +9,51 @@ const Definition = () => {
   let { search } = useParams();
   const navigate = useNavigate();
   const [notFound, setNotFound] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
-    fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + search)
+    // const url = "https://httpstat.us/501";
+    const url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + search;
+
+    fetch(url)
       .then((response) => {
         if (response.status === 404) {
           setNotFound(true);
+        } else if (!response.ok) {
+          setError(true);
+          throw new Error("Something went wrong!");
         }
         return response.json();
       })
       .then((data) => {
         setWord(data[0].meanings);
+      })
+      .catch((e) => {
+        console.log(e.message);
       });
   }, []);
 
   if (notFound === true) {
     return (
-      <>
+      <div>
         <NotFound />
         <Link to="/dictionary">Search another</Link>
-      </>
+      </div>
+    );
+  }
+
+  if (error === true) {
+    return (
+      <div>
+        <p>Something went wrong! Try again.</p>
+        <Link to="/dictionary">Search another</Link>
+      </div>
     );
   }
   return (
-    <>
+    <div>
       {word ? (
-        <>
+        <div>
           <h1>Here is the definition:</h1>
           {word.map((meaning) => {
             return (
@@ -44,11 +63,11 @@ const Definition = () => {
               </p>
             );
           })}
-        </>
+        </div>
       ) : (
         <p>Loading...</p>
       )}
-    </>
+    </div>
   );
 };
 
